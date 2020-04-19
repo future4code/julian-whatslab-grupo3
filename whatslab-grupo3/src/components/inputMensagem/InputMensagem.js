ca6bbc67577c54abd25b2ce9258f96509c87238d
Hugo Lacerda <hugolacerdar@gmail.com>
2020-04-19 19:26:44 -0300

    possivelmente ok

--- whatslab-grupo3/src/components/inputMensagem/InputMensagem.js
+++ whatslab-grupo3/src/components/inputMensagem/InputMensagem.js
@@ -71,7 +71,7 @@ let uniqueKey = 0;
 class InputMensagem extends React.Component{
     state = {
         mensagens: [
-            
+
         ],
 
         valorInputNome: "",
@@ -125,7 +125,6 @@ class InputMensagem extends React.Component{
 
     };
 
-<<<<<<< HEAD
     removeMensagem = (e) =>{
 
         const key = e.target.getAttribute("data-key");
@@ -135,27 +134,7 @@ class InputMensagem extends React.Component{
             return mensagem.key !== key;
         })})
     }
-
-=======
-    //onkeydown ainda não funciona
-    onKeyDownEnter = (event) => {
-        if (event.key === "Enter") {
-            this.adicionaMensagem();
-        }
-    }
-
-    //doubleclick ainda não funciona
-    removeMensagem = (mensagemIndex) => {
-        if (window.confirm("Tem certeza que deseja deletar essa mensagem?")) {
-            const mensagemRemovida = this.state.mensagens;
-
-            mensagemRemovida.splice(mensagemIndex, 1);
-        }
-
-        this.setState({ mensagens: mensagemRemovida });
->>>>>>> cb523b22f648c6eedbfd2b887c80431470aeb5f4
-    }
-
+}
     render(){
         const listaDeMensagens = this.state.mensagens.map((mensagem) => {
             if(mensagem.souEu){
@@ -175,15 +154,9 @@ class InputMensagem extends React.Component{
                 {listaDeMensagens}
                 </Mensagem>
                 <Formulario>
-<<<<<<< HEAD
                     <InputNome value={this.state.valorInputNome} placeholder="Usuário" onChange={this.onChangeInputNome} />
                     <InputTexto value={this.state.valorInputTexto} placeholder="Mensagem" onChange={this.onChangeInputTexto} onKeyPress={this.onKeyEnter} />
                     <BotaoEnviar onClick={this.adicionaMensagem}>Enviar</BotaoEnviar>
-=======
-                    <input value={this.state.valorInputNome} placeholder="Usuário" onChange={this.onChangeInputNome} />
-                    <input value={this.state.valorInputTexto} placeholder="Mensagem" onChange={this.onChangeInputTexto} />
-                    <button onClick={this.adicionaMensagem} onKeyDown={this.onKeyDownEnter} onDoubleClick={this.removeMensagem}>Enviar</button>
->>>>>>> cb523b22f648c6eedbfd2b887c80431470aeb5f4
                 </Formulario>
                 </div>
         )
